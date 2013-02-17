e78fb36dfc89e10e21bd1bcd4a2c1c665ee68db4
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-17 19:59:54 +0900

    Create BASIC project.

---
+++ BasicIOSProjectWithCoreData/BasicIOSProjectWithCoreData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BasicIOSProjectWithCoreData
+//
+//  Created by Matsuki Hidenori on 2/17/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
