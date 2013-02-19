27d7d6c427315d21f3bac19132fa8e3189c11a09
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-20 05:45:44 +0900

    Rename entity name.

---
+++ BasicIOSProjectWithCoreData/BasicIOSProjectWithCoreData/TaskEntity.m
@@ -0,0 +1,16 @@
+//
+//  Task.m
+//  BasicIOSProjectWithCoreData
+//
+//  Created by MATSUKI Hidenori on 2/19/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import "TaskEntity.h"
+
+
+@implementation TaskEntity
+
+@dynamic title;
+
+@end
